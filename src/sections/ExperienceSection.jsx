3cc87998aa183ae7546af1080742668ff096b75e
CH Umar Aslam <chumaraslam22@gmail.com>
2024-04-20 15:01:50 +0500

    final

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -7,9 +7,18 @@ const Experience = () => {
       <div className="border-l-4 border-blue-500 pl-4">
         <h3 className="text-xl font-semibold text-gray-800">React.js Developer at <a href="https://site.recgenz.com" className="text-blue-600 hover:underline">RecGenz</a></h3>
         <p className="text-gray-600 mb-2">January 2024 - Present</p>
-        <p className="text-gray-700">
-          As a React.js Developer at RecGenz, I've been instrumental in shaping the digital experience. Leveraging the power of Tailwind CSS, I've crafted stunning and responsive designs that guarantee a visually delightful user experience. Additionally, by integrating Node Mailer, I've engineered seamless email communication within the application, significantly enhancing user engagement and retention. My expertise in leveraging a variety of external libraries has enriched functionality and guaranteed scalability, empowering the team to innovate swiftly. One of my key achievements has been spearheading the development of an eye-catching and responsive portfolio section, showcasing our projects with elegance and professionalism.
-        </p>
+
+        <ul className='pl-1 text-pretty'>
+          <li> <span className='mx-2 '>- </span> Led the development of RecGenz’s company website using React and Tailwind CSS for optimal functionality and design consistency.</li>
+          <li> <span className='mx-2 '>- </span>Designed dynamic user interfaces with advanced React techniques to enhance site interactivity and speed.</li>
+          <li> <span className='mx-2 '>- </span>Ensured website maintained a cohesive visual style through Tailwind CSS.</li>
+          <li> <span className='mx-2 '>- </span>Prioritized thorough testing and diligent bug fixing to guarantee a flawless user experience.</li>
+          <li> <span className='mx-2 '>- </span>Collaborated effectively with cross-functional teams, leveraging tools like Figma to streamline project workflows.</li>
+          <li> <span className='mx-2 '>- </span>Engaged deeply with the full front-end development lifecycle, significantly improving React skills.</li>
+          <li> <span className='mx-2 '>- </span>Contributions instrumental in establishing and upholding RecGenz's strong online presence.</li>
+        </ul>
+
+
       </div>
     </div>
   );
